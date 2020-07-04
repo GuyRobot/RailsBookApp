49941e99bc157a97bf0442774b673e1c7e525fd0
GuysRobot <bluexker@gmail.com>
2020-07-04 06:04:45 -0400

    Add Models And Configure Views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,20 @@
+# frozen_string_literal: true
+
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'home#index'
+  resources :books do
+    member do
+      put 'add', to: 'books#library'
+      put 'remove', to: 'books#library'
+    end
+  end
+  resources :library, only: [:index]
+  resources :pricing, only: [:index]
+  resources :subscriptions
+
+  devise_for :users, controller: { registrations: 'registrations' }
+  root to: 'book#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
